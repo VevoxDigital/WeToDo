4f8ed7925ddf089bcfdbcd1fd1a7c27d58abf160
Matthew Struble <matt@vevox.io>
2017-04-08 18:17:46 -0700

    tests for new lists

--- test/lib/list.js
+++ test/lib/list.js
@@ -4,22 +4,16 @@ const expect = require('expect.js')
 
 const lists = require('../../src/app/js/lib/list')
 
-const ENTRY_STRING = '1234 CREATE g:123 fff'
-const ENTRY_DATA = { time: new Date(1234), command: 'CREATE', user: 'g:123', data: 'fff' }
+const ENTRY_STRING = '1234 CREATE g:123 note|fff'
+const ENTRY_DATA = { time: new Date(1234), command: 'CREATE', user: 'g:123', data: 'note|fff' }
 
 const LIST_STRING = 'Example List\ngh:1234\n' + ENTRY_STRING
 const LIST_DATA = { name: 'Example List', user: 'gh:1234' }
 
-describe('ListModificationEntry', () => {
-  let list
-
-  beforeEach(() => {
-    list = new lists.List(LIST_DATA)
-  })
-
+describe('ListModification', () => {
   describe('pattern', () => {
     it('should parse a entry string', () => {
-      const data = lists.ListModificationEntry.pattern.exec(ENTRY_STRING)
+      const data = lists.ListModification.pattern.exec(ENTRY_STRING)
 
       expect(data[1]).to.be('' + ENTRY_DATA.time.getTime())
       expect(data[2]).to.be(ENTRY_DATA.command)
@@ -29,127 +23,225 @@ describe('ListModificationEntry', () => {
   })
 
   describe('<init>', () => {
-    it('should fail if a list is not provided', () => {
+    it('should fail if input is non-string', () => {
       try {
-        const e = new lists.ListModificationEntry()
+        const e = new lists.ListModification()
         expect().fail(e)
       } catch (e) {
-        expect(e.message.match(/^Given list/)).to.be.ok()
+        expect(e.message).to.match(/^expected string data/)
       }
     })
 
     it('should parse data', () => {
-      const entry = new lists.ListModificationEntry(list, ENTRY_STRING)
+      const entry = new lists.ListModification(ENTRY_STRING)
 
       expect(entry.time).to.be.a(Date)
       expect(entry.time.getTime()).to.be(ENTRY_DATA.time.getTime())
 
-      expect(entry.command).to.be(ENTRY_DATA.command)
+      expect(entry.handler.command).to.be(ENTRY_DATA.command)
       expect(entry.user).to.be(ENTRY_DATA.user)
       expect(entry.data).to.be(ENTRY_DATA.data)
     })
 
-    it('should throw an error if data is neither string nor object', () => {
-      try {
-        const e = new lists.ListModificationEntry(list, 0)
-        expect.fail(e)
-      } catch (e) {
-        expect(e.message).to.match(/^data must be/)
-      }
-    })
-
     it('should throw an error if the entry string is invalid', () => {
       try {
-        const e = new lists.ListModificationEntry(list, 'foo')
+        const e = new lists.ListModification('foo')
         expect.fail(e)
       } catch (e) {
-        expect(e.message).to.match(/^Bad entry line/)
+        expect(e.message).to.match(/^bad entry line/)
       }
     })
 
     it('should throw an error if the command is unknown', () => {
       try {
-        const e = new lists.ListModificationEntry(list, '1 FOO f:1 bar')
+        const e = new lists.ListModification('1 FOO f:1 bar')
         expect.fail(e)
       } catch (e) {
-        expect(e.message).to.be('Unknown command')
+        expect(e.message).to.match(/^unknown command/)
       }
     })
   })
 
   describe('#resolveUser()', () => {
-    let entry
-
-    beforeEach(() => {
-      entry = new lists.ListModificationEntry(list, ENTRY_STRING)
-    })
-
     it('should return a promise', () => {
+      const entry = new lists.ListModification(ENTRY_STRING)
+
       expect(entry.resolveUser()).to.be.a(Promise)
     })
   })
 
-  describe('#toString()', () => {
-    let entry
+  describe('#apply()', () => {
+    it('should execute handler with target and data', () => {
+      const entry = new lists.ListModification(ENTRY_STRING)
 
-    beforeEach(() => {
-      entry = new lists.ListModificationEntry(list, ENTRY_STRING)
+      const list = new lists.List('foo', 'bar')
+      entry.apply(list) // the CREATE command is in the ENTRY_STRING input
+
+      expect(list.entries.length).to.be(1)
     })
+  })
 
+  describe('#toString()', () => {
     it('should serialize into input data', () => {
+      const entry = new lists.ListModification(ENTRY_STRING)
       expect(entry.toString()).to.be(ENTRY_STRING)
     })
   })
 })
 
-describe('List', () => {
+describe('ListEntry', () => {
   describe('<init>', () => {
-    it('should parse string into list data', () => {
-      const l = new lists.List(LIST_STRING)
+    it('should create entry from data', () => {
+      const title = 'Foo'
+      const type = 'note'
 
-      expect(l.name).to.be(LIST_DATA.name)
-      expect(l.users[0]).to.be(LIST_DATA.user)
-      expect(l.entries.length).to.be(1)
+      const entry = new lists.ListEntry(type, title)
+
+      expect(entry.type).to.be(type)
+      expect(entry.title).to.be(title)
     })
 
-    it('should create empty objects if not supplied', () => {
-      const l = new lists.List({ name: 'foo' })
+    it('should throw an error if type is non-string', () => {
+      try {
+        const entry = new lists.ListEntry()
+        expect.fail(entry)
+      } catch (e) {
+        expect(e.message).to.match(/^expected string type/)
+      }
+    })
 
-      expect(l.users).to.be.an(Array)
-      expect(l.users.length).to.be(0)
-      expect(l.entries).to.be.an(Array)
-      expect(l.entries.length).to.be(0)
+    it('should throw an error if title is non-string', () => {
+      try {
+        const entry = new lists.ListEntry('')
+        expect.fail(entry)
+      } catch (e) {
+        expect(e.message).to.match(/^expected string title/)
+      }
+    })
+  })
+})
+
+describe('List', () => {
+  describe('parseList', () => {
+    it('should create a list', () => {
+      const list = lists.List.parseList('foo', LIST_STRING)
+
+      expect(list.uuid).to.be('foo')
+      expect(list.title).to.be(LIST_DATA.name)
+
+      expect(list.users.length).to.be(1)
+      expect(list.modifications.length).to.be(1)
     })
   })
 
-  describe('#addEntry', () => {
+  describe('<init>', () => {
+    it('should create a list', () => {
+      const list = new lists.List(undefined, LIST_DATA.name, LIST_DATA.user)
+
+      expect(list.users).to.be.an(Array)
+      expect(list.users[0]).to.be(LIST_DATA.user)
+
+      expect(list.title).to.be(LIST_DATA.name)
+    })
+  })
+
+  describe('#addModification()', () => {
     let list
 
-    beforeEach(() => {
-      list = new lists.List(LIST_DATA)
+    before(() => {
+      list = new lists.List(undefined, 'Title')
     })
 
-    it('should add an entry to the list', () => {
-      list.addEntry(new lists.ListModificationEntry(list, ENTRY_DATA))
-      expect(list.entries.length).to.be(1)
+    it('should add a mod to the list', () => {
+      list.addModification(new lists.ListModification('33 CREATE gh:123 note|foo'))
+
+      expect(list.modifications.length).to.be(1)
     })
 
-    it('should throw an error if input is not a list', () => {
-      expect(list.addEntry)
-        .to.throwException(/^Entry must/)
+    it('should sort the modifications by time', () => {
+      list.addModification(new lists.ListModification('22 CREATE gh:123 note|foo'))
+      // same list as previous test, so we have two modifications now
+
+      expect(list.modifications.length).to.be(2)
+      expect(list.modifications[0].time.getTime()).to.be(22)
     })
   })
 
-  describe('#resolveUsers', () => {
+  describe('#addEntry()', () => {
+    it('should add an entry to the list', () => {
+      const list = new lists.List(undefined, '')
+      list.addEntry(new lists.ListEntry('foo', 'note'))
+
+      expect(list.entries.length).to.be(1)
+    })
+  })
+
+  describe('#apply()', () => {
+    it('should call the apply method for modification at index', () => {
+      const list = new lists.List(undefined, '')
+      list.addModification(new lists.ListModification(ENTRY_STRING))
+      list.apply(0)
+
+      expect(list.entries.length).to.be(1)
+    })
+  })
+
+  describe('#applyFrom()', () => {
+    it('should call apply for all modifications', () => {
+      const list = new lists.List(undefined, '')
+
+      list.addModification(new lists.ListModification(ENTRY_STRING))
+      list.addModification(new lists.ListModification(ENTRY_STRING))
+
+      let i = 0
+      list.apply = index => {
+        expect(index).to.be(i++)
+      }
+
+      list.applyFrom()
+    })
+  })
+
+  describe('#reset()', () => {
+    it('should empty entries and call #applyFrom()', () => {
+      const list = new lists.List(undefined, '')
+      list.addEntry(new lists.ListEntry('foo', 'note'))
+
+      expect(list.entries.length).to.be(1)
+
+      list.applyFrom = () => {
+        expect(list.entries.length).to.be(0)
+      }
+
+      list.reset()
+    })
+  })
+
+  describe('#resolveUsers()', () => {
     it('should return a promise', () => {
-      const list = new lists.List(LIST_DATA)
+      const list = new lists.List(undefined, '')
+
       expect(list.resolveUsers()).to.be.a(Promise)
     })
   })
 
-  describe('#toString', () => {
+  describe('#isShared', () => {
+    it('should return true if and only if there are multiple users', () => {
+      const list = new lists.List(undefined, '', 'u1')
+
+      expect(list.isShared()).to.be(false)
+
+      list._users.push('u2')
+
+      expect(list.isShared()).to.be(true)
+    })
+  })
+
+  describe('#toString()', () => {
     it('should serialize the list', () => {
-      const list = new lists.List(LIST_STRING)
+      const list = new lists.List(undefined, LIST_DATA.name, LIST_DATA.user)
+      list.addModification(new lists.ListModification(ENTRY_STRING))
+
       expect(list.toString()).to.be(LIST_STRING)
     })
   })
